40070d51a20aa9ca4b0819292fb74dac91339717
Local Admin <ladmin@Leadership-Learning-Higher-Ed-98.local>
2016-09-09 14:38:49 -0500

    First commit to start project
    jsincludes.html:
    This file can be used to run and test javascript code until our GUI is done. Just add your .js file and update this .html file so you can run JS from your browser.
    
    Event.js:
    This file is for the event object construction and some methods to use its data.

---
+++ Event.js
@@ -0,0 +1,7 @@
+var Event = function(name,date,time,duration,location){
+    this.name = name;
+    this.date = date;
+    this.time = time;
+    this.duration = duration;
+    this.location = location;
+}
